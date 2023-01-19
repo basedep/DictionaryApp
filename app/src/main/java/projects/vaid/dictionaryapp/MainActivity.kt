3f46145211713dafa72f5670a68b25ed27c3f260
Vaid Sjothonov <mode_private@mail.ru>
2023-01-19 23:32:46 +0300

    Add style for SearchView in search_fragment.xml

--- app/src/main/java/projects/vaid/dictionaryapp/MainActivity.kt
+++ app/src/main/java/projects/vaid/dictionaryapp/MainActivity.kt
@@ -2,10 +2,16 @@ package projects.vaid.dictionaryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import projects.vaid.dictionaryapp.ui.SearchFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.container, SearchFragment())
+            .commit()
     }
 }
---
+++ app/src/main/java/projects/vaid/dictionaryapp/ui/SearchFragment.kt
@@ -0,0 +1,18 @@
+package projects.vaid.dictionaryapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import projects.vaid.dictionaryapp.R
+
+class SearchFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+       val view = inflater.inflate(R.layout.search_fragment, container, false)
+        return view
+    }
+
+}
